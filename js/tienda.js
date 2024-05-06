eae894bc5201ab5e988e6cdd1b4f7b0b599e975f
PolloLopez <pollolopeza@gmail.com>
2024-05-06 16:49:56 -0300

    index y tienda OK

--- js/tienda.js
+++ js/tienda.js
@@ -4,7 +4,7 @@ let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 let productos = [];
 
 /*traer de base de datos local */
-fetch("/practicaJS/data/productos.json")
+fetch("/trabajoFinalJS/data/productos.json")
     .then(res => res.json())
     .then(data => {
         productos = data;
